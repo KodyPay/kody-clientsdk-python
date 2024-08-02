f8af8c256160935316be581c31259cf7b229ece0
Andrea Fabris <andrea.fabris@kodypay.com>
2024-08-02 11:31:57 +0100

    Fix sed command

--- actions.sh
+++ actions.sh
@@ -8,6 +8,7 @@ find ./proto -name "*.proto" -print0 | xargs -0 python -m grpc_tools.protoc -I./
 rm -rf proto
 find "generated/com/kodypay/grpc/" -type d -not -name '__pycache__' -exec touch {}/__init__.py \;
 echo "__version__ = '0.0.1'" > generated/com/kodypay/grpc/__init__.py
-cp -R generated/com/kodypay/grpc/* ./kody_clientsdk_python
+mkdir kody_clientsdk_python
+cp -R generated/com/kodypay/grpc/* kody_clientsdk_python
 rm -rf generated
 find ./kody_clientsdk_python -type f -name "*.py" -exec sed -i '' -e '/^import / s/com\.kodypay\.grpc/kody_clientsdk_python/g' -e '/^from / s/com\.kodypay\.grpc/kody_clientsdk_python/g' {} \;
