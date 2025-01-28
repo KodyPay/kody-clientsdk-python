ba2b595a4464f96bc04533a9edc391aeb661d88f
ioannis-kody <ioannis.petridis@kody.com>
2025-01-28 09:48:16 +0000

    Include accepts_only fields (#25)
    * Updated 3.6 to align with 3.12. Included accepts only field and used payment data object
    
    * Include date_paid

--- versions/3_12/samples/terminal/process_payment.py
+++ versions/3_12/samples/terminal/process_payment.py
@@ -17,9 +17,12 @@ def send_terminal_payment():
 
     show_tips = True
     payment_method = kody_model.PaymentMethod(
-        # Options: CARD, ALIPAY, WECHAT
+        # Options: CARD, E-WALLET
         payment_method_type="CARD"
     )
+
+    # Options: VISA, MASTERCARD, AMEX, BAN_CONTACT, CHINA_UNION_PAY, MAESTRO, DINERS = 6, DISCOVER, JCB, ALIPAY, WECHAT
+    accepts_only=["VISA", "MASTERCARD", "AMEX"]
 
     with grpc.secure_channel(target=address, credentials=grpc.ssl_channel_credentials()) as channel:
         client = kody_client.KodyPayTerminalServiceStub(channel)
@@ -29,23 +32,28 @@ def send_terminal_payment():
                 terminal_id=terminal_id,
                 amount=amount,
                 show_tips=show_tips,
-                payment_method=payment_method
+                payment_method=payment_method,
+                accepts_only = accepts_only
             ),
             metadata=[("x-api-key", api_key)]
         )
 
         for response in response_iterator:
-            print(f"Order ID: {response.order_id}")
+            print(f"Order ID: {response.idempotency_uuid}")
             if response.status:
                 status = kody_model.PaymentStatus.Name(response.status)
                 print(f"Status: {status}")
-                print(f"Receipt JSON: {response.receipt_json}")
+                print(f"Receipt JSON: {response.payment_data.receipt_json}")
                 print(f"Date Created: {datetime.fromtimestamp(response.date_created.seconds)}")
-                print(f"Total Amount: {response.total_amount}")
-                print(f"Sale Amount: {response.sale_amount}")
-                print(f"Tips Amount: {response.tips_amount}")
+                print(f"Date Paid: {datetime.fromtimestamp(response.payment_data.date_paid.seconds)}")
+                print(f"Total Amount: {response.payment_data.total_amount}")
+                print(f"Sale Amount: {response.payment_data.sale_amount}")
+                print(f"Tips Amount: {response.payment_data.tips_amount}")
+                print(f"Payment method type: {kody_model.PaymentMethodType.Name(response.payment_data.payment_method_type)}")
+                print("Accepts only: " + ', '.join(kody_model.PayRequest.PaymentMethods.Name(paymentMethod) for paymentMethod in response.accepts_only))
+
                 if status == "SUCCESS":
-                    print(f"External Payment Reference: {response.ext_payment_ref}")
+                    print(f"PSP Reference: {response.psp_reference}")
                 elif status == "FAILED" or status == "CANCELLED":
                     print(f"Failure Reason: {response.failure_reason}")
 
